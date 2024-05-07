9ec60868118a716898718d33290888da559c012d
Usman Khan <30685514+usmankhan7867@users.noreply.github.com>
2024-05-08 03:49:06 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -71,7 +71,7 @@ $table_prefix = 'wpqj_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 /* Add any custom values between this line and the "stop editing" line. */
 /* That's all, stop editing! Happy publishing. */
 /** Absolute path to the WordPress directory. */
